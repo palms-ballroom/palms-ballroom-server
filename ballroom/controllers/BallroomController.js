333148aa6634a1806094ad30fa37931d2e41572f
Muhammad Ihsan Erdiansyah <Ihsan.erdi97@gmail.com>
2022-04-12 23:33:08 +0700

    CRUD done for now

--- ballroom/controllers/BallroomController.js
+++ ballroom/controllers/BallroomController.js
@@ -12,6 +12,7 @@ class Controller {
   static async getOne(req, res, next) {
     try {
       const ballroom = await Ballroom.findOne(req.params.hotelApiId);
+      console.log(req.params.hotelApiId);
       res.status(200).json(ballroom);
     } catch (error) {
       next(error);
@@ -27,14 +28,21 @@ class Controller {
   }
   static async create(req, res, next) {
     try {
-      const { name, status, price, images, city, hotelApiId } = req.body;
+      const { hotelApiId, userId, name, pricePerHour, pricePerDay, mainImg, images1, images2, images3, images4, clicked, city } = req.body;
+      console.log(req.body);
       const ballroom = await Ballroom.create({
+        hotelApiId: +hotelApiId,
+        userId: +userId,
         name,
-        status,
-        price,
-        images,
+        pricePerHour: +pricePerHour,
+        pricePerDay: +pricePerDay,
+        mainImg,
+        images1,
+        images2,
+        images3,
+        images4,
+        clicked: +clicked,
         city,
-        hotelApiId,
       });
       res.status(201).json(ballroom);
     } catch (error) {
@@ -51,6 +59,14 @@ class Controller {
         images,
         city,
       });
+      res.status(200).json(ballroom);
+    } catch (error) {
+      next(error);
+    }
+  }
+  static async delete(req, res, next) {
+    try {
+      const ballroom = await Ballroom.delete(req.params.hotelApiId);
       res.status(200).json(ballroom);
     } catch (error) {
       next(error);
