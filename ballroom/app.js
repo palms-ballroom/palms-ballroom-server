1ce91bf5fe4c2d78aef9420c935e776a04d4aabc
Muhammad Ihsan Erdiansyah <Ihsan.erdi97@gmail.com>
2022-04-12 12:31:46 +0700

    Connect MongoDB

---
+++ ballroom/app.js
@@ -0,0 +1,28 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 4001;
+const { connect } = require("./config/mongodb");
+// const userRouter = require("./routes/userRoutes");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.status(200).json("This is mongoDB REST API by PALMS");
+});
+
+connect()
+  .then((db) => {
+    console.log("db: ", db);
+    app.listen(port, () => {
+      console.log(`Example app listening on port http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
